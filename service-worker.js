d7a351f07219ba1d4ee2df9712e79793d324d926
AhmadZahran1011 <zahranahmad91@gmail.com>
2025-10-06 21:38:29 +0700

    Tugas Desain Web

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ const CACHE_NAME = "zahran-network-v1"
 const OFFLINE_URL = "/offline.html"
 
 const urlTcoCache = [
-    "./Index.html",
+    "./index.html",
     "./about.html",
     "./contact.html",
     "./offline.html",
