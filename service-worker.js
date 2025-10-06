46f47517411a20c76f5045869ebbd13abc32274b
AhmadZahran1011 <zahranahmad91@gmail.com>
2025-10-06 22:45:13 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = "zahran-network-v2"
+const OFFLINE_URL = "/offline.html"
+
+const urlsToCache = [
+    "./index.html",
+    "./about.html",
+    "./contact.html",
+    "./offline.html",
+    "./Edit.css",
+    "./About.css",
+    "./Artboard 3.png",
+    "./Content.css"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Service Worker: Caching files");
+            return cache.addAll(urlsToCache);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+  console.log("Service Worker activated");
+});
+
+// Fetch Request
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // jika ada di cache, tampilkan dari cache
+      if (response) {
+        return response;
+      }
+      // jika tidak ada di cache, ambil dari network
+      return fetch(event.request).catch(() => {
+        // jika gagal ambil dari network (offline), tampilkan offline.html
+        if (event.request.mode === "navigate") {
+          return caches.match(OFFLINE_URL);
+        }
+      });
+    })
+  );
+});
