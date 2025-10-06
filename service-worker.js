303d7a5328250c52db95cc3fa03906d13c11bfe9
AhmadZahran1011 <zahranahmad91@gmail.com>
2025-10-06 22:51:26 +0700

    Tugas Desain Web

--- service-worker.js
+++ service-worker.js
@@ -11,44 +11,34 @@ const urlsToCache = [
     "./Artboard 3.png",
     "./Content.css"
 ];
-
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log("Service Worker: Caching files");
-            return cache.addAll(urlsToCache);
-        })
-    );
-    self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      );
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
-  console.log("Service Worker activated");
 });
 
-// Fetch Request
-self.addEventListener("fetch", event => {
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            Promise.all(
+                cacheNames.map((cache) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            )
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      // jika ada di cache, tampilkan dari cache
-      if (response) {
-        return response;
-      }
-      // jika tidak ada di cache, ambil dari network
-      return fetch(event.request).catch(() => {
-        // jika gagal ambil dari network (offline), tampilkan offline.html
-        if (event.request.mode === "navigate") {
-          return caches.match(OFFLINE_URL);
-        }
+    fetch(event.request).catch(() => {
+      
+      return caches.match(event.request).then((response) => {
+        return response || caches.match("./offline.html");
       });
     })
   );
