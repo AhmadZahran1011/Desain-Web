cbec16b84f111dc9287f0d8469a0f768c4da3ac3
AhmadZahran1011 <zahranahmad91@gmail.com>
2025-10-12 06:25:52 +0700

    Upload

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,48 @@
-const CACHE_NAME = "zahran-network-v1"
-const OFFLINE_URL = "/offline.html"
+const CACHE_NAME = "zahran-network-v1";
+const OFFLINE_URL = "./offline.html";
 
 const urlsToCache = [
-    "./index.html",
-    "./about.html",
-    "./contact.html",
-    "./offline.html",
-    "./Edit.css",
-    "./About.css",
-    "./Artboard 3.png",
-    "./Content.css"
+  "./",
+  "./index.html",
+  "./about.html",
+  "./contact.html",
+  "./offline.html",
+  "./Edit.css",
+  "./About.css",
+  "./Content.css",
+  "./Artboard 3.png"
 ];
-self.addEventListener('install', (event) => {
+
+// Instalasi Service Worker dan caching awal
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// Aktivasi Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
+  self.clients.claim();
 });
 
-self.addEventListener("activate", (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            Promise.all(
-                cacheNames.map((cache) => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
-                })
-            )
-        })
-    );
-});
-
+// Intersep permintaan fetch
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     fetch(event.request).catch(() => {
-      
       return caches.match(event.request).then((response) => {
-        return response || caches.match("./offline.html");
+        return response || caches.match(OFFLINE_URL);
       });
     })
   );
